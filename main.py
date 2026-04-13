55066bb03174525490ec4ee3bb73cc2a976263d3
Keith <keithjayjones@gmail.com>
2026-04-13 14:16:31 -0400

    Add arena route

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask, jsonify, request
 import requests
 import json
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
-API_KEY = os.environ.get("API_KEY")
+API_KEY = os.getenv("API_KEY")
+ROLE_ID = os.getenv("ROLE_ID", None)
 
 def require_api_key(f):
     def wrapper(*args, **kwargs):
@@ -18,14 +21,13 @@ def require_api_key(f):
     return wrapper
 
 
-def send_discord_webhook(webhook_url, message, role_id=None, username="Bot", avatar_url=None):
-
-    if not webhook_url.startswith("https://discord.com/api/webhooks/"):
+def send_discord_webhook(webhook_url, message, username="Bot", avatar_url=None):
+    if not webhook_url or not webhook_url.startswith("https://discord.com/api/webhooks/"):
         raise ValueError("Invalid Discord webhook URL.")
 
     # If a role ID is provided, format the mention
-    if role_id:
-        message = f"<@&{role_id}> {message}"
+    if ROLE_ID:
+        message = f"<@&{ROLE_ID}> {message}"
 
     payload = {
         "content": message,
@@ -52,7 +54,19 @@ def send_discord_webhook(webhook_url, message, role_id=None, username="Bot", ava
 def index():
     return jsonify({"message": "Hello, World!", "status": "ok"})
 
+@app.route("/arena", methods=["POST"])
+@require_api_key
+def arena():
+    send_discord_webhook(
+        webhook_url=os.getenv("ARENA_WEBHOOK_URL", ""),
+        message="🔥 Arena reset in 15 mins! ⏰⚔️",
+        username="Arena Reminder",
+        avatar_url="https://i.postimg.cc/SN2jZmR5/REMINDER.png"
+    )
+
+    return "Arena notification sent successfully."
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
